cdac09d43f530a8c05cd6afd39a70a5f1dc81a77
Xuzheng Lu <xuzheng_lu@gwu.edu>
2020-09-14 15:21:52 -0400

    Phase 2 Done

--- parallel/sum.go
+++ parallel/sum.go
@@ -16,33 +16,50 @@ import (
 // and launch `goRoutineNums` go routines to do calculation
 // return sum of these Integers
 // You Must start exact `goRoutineNums` go routines or you lose points here
-func SumRoutine(nums []int, c chan int) int {
+func SumRoutine(nums []int, c chan int) {
+	// Calculate sum for each go routine
 	sum := 0
-	for _, v := range s {
+	for _, v := range nums {
 		sum += v
 	}
-	return sum
+	c <- sum
 }
 
 func Sum(goRoutineNum int, fileName string) int {
 	//TODO Add your code here
+	if goRoutineNum < 1 {
+		log.Println("goRoutineNum must >= 1!")
+	}
+
 	nums, err := readInts(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	chunkSize := len(nums) / goRoutineNum
+
+	// Create channel with size of go routine number
+	ch := make(chan int, goRoutineNum)
+
+	// Calculate chunk size for each go routine
+	var chunkSize int
+	if len(nums)%goRoutineNum == 0 {
+		chunkSize = len(nums) / goRoutineNum
+	} else {
+		chunkSize = len(nums)/goRoutineNum + 1
+	}
+
+	// Create go routines
 	for i := 0; i < len(nums); i += chunkSize {
 		end := i + chunkSize
 		if end > len(nums) {
 			end = len(nums)
 		}
-		chunk := nums[i:end]
-		go SumRoutine(chunk)
+		go SumRoutine(nums[i:end], ch)
 	}
 
+	// Receive value from the channel and calculate the sum of go routines
 	sum := 0
-	for i := 0; i < len(nums); i++ {
-		sum += nums[i]
+	for i := 0; i < goRoutineNum; i++ {
+		sum += <-ch
 	}
 	return sum
 }
--- parallel/sum_test.go
+++ parallel/sum_test.go
@@ -14,7 +14,7 @@ func test(t *testing.T, fileName string, num int, expected int) {
 	res := Sum(num, fileName)
 	if res != expected {
 		t.Fatal(fmt.Sprintf(
-			"Test %s Failed: expected %d, got %d\n", fileName,expected, res))
+			"Test %s Failed: expected %d, got %d\n", fileName, expected, res))
 	}
 }
 
@@ -27,20 +27,27 @@ func Test1_2(t *testing.T) {
 	test(t, "test1.txt", 10, -83)
 }
 
-func Test2_1(t *testing.T)  {
+func Test2_1(t *testing.T) {
 	test(t, "test2.txt", 1, 19605)
 
 }
 
-func Test2_2(t *testing.T)  {
+func Test2_2(t *testing.T) {
 	test(t, "test2.txt", 10, 19605)
 }
 
-func Test3_1(t *testing.T)  {
+func Test3_1(t *testing.T) {
 	test(t, "test3.txt", 5, 31418)
 }
 
-func Test3_2(t *testing.T)  {
+func Test3_2(t *testing.T) {
 	test(t, "test3.txt", 20, 31418)
 }
 
+func Test4_1(t *testing.T) {
+	test(t, "test4.txt", 2, 10)
+}
+
+func Test4_2(t *testing.T) {
+	test(t, "test4.txt", 4, 10)
+}
